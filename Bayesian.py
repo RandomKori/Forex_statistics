515f5fc03e229b9892764301fa6b9debd8dfea85
Grigoriy Chaunin <randomkori@live.com>
2019-11-29 06:45:28 +0600

    v22

--- Bayesian.py
+++ Bayesian.py
@@ -28,7 +28,7 @@ def analyze_standard(data):
         cov = pm.Deterministic('cov', covariance(sigma_p, rho_p))
         cov_p = cov
 
-        mult_norm = pm.MvNormal('mult_norm', mu=[10.1, 79.],  # set mu to median
+        mult_norm = pm.MvNormal('mult_norm', mu=[-100, 100],  # set mu to median
                                 cov=cov_p, observed=data.T)
 
     return model
@@ -38,23 +38,23 @@ def analyze_robust(data):
     with pm.Model() as model:
         # priors
         mu = pm.Normal('mu', mu=0., tau=0.000001, shape=2,
-                       testval=np.array([10.1, 79.]))  # set mu to median
-        sigma = pm.Uniform('sigma', lower=0, upper=1000, shape=2,
-                           testval=np.array([0.133434, 5.9304]),  # init with mad
-                           transform='interval')
+                       testval=np.array([-100, 100]))  # set mu to median
+        sigma = pm.Uniform('sigma', lower=0, upper=0.001, shape=2,
+                           testval=np.array([0.0001, 0.001]),  # init with mad
+                           transform=None)
         rho = pm.Uniform('r', lower=-1, upper=1,
                          testval=-0.2144021,  # init with Spearman's correlation
                          transform=None)
 
         # print values for debugging
-        rho_p = Print('rho')(rho)
-        sigma_p = Print('sigma')(sigma)
+        rho_p = rho
+        sigma_p = sigma
 
         cov = pm.Deterministic('cov', covariance(sigma_p, rho_p))
         num = pm.Exponential('nu_minus_one', lam=1. / 29., testval=1)
         nu = pm.Deterministic('nu', num + 1)
-        cov_p = Print('cov')(cov)
-        nu_p = Print('nu')(nu)
+        cov_p = cov
+        nu_p = nu
 
         mult_norm = pm.MvStudentT('mult_norm', nu=nu_p, mu=mu,
                                   Sigma=cov_p, observed=data.T)
