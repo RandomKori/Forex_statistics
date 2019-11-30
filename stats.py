98df30ff6a9ca34a705036fe8b893f4eb96db6ca
Grigoriy Chaunin <randomkori@live.com>
2019-11-30 21:13:26 +0600

    v35

--- stats.py
+++ stats.py
@@ -7,3 +7,20 @@ def cauchyvar(data):
         beta=pm.Uniform('beta',lower=0.0,upper=200.0)
         cauchy=pm.Cauchy('cauchy',alpha=alfa,beta=beta,observed=data)
     return model
+
+def verbar(data):
+    v=0
+    n=0
+    ni=0
+    for i in range(len(data)):
+        if data[i]==0.0:
+            ni=ni+1
+        else:
+            if data[i]>0.0:
+                v=v+1
+            else:
+                n=n+1
+    vr=v/len(data)
+    nr=n/len(data)
+    nir=ni/len(data)
+    return vr,nr,nir
